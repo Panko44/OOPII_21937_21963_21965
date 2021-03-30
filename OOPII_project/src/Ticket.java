e2c870c1c5bcdffdf99a5cd71b403edcd41ed762
Rafail Ntymenos <Rafail Ntymensos@tele>
2021-03-30 20:38:27 +0300

    free ticket

--- OOPII_project/src/Ticket.java
+++ OOPII_project/src/Ticket.java
@@ -2,15 +2,18 @@ import java.util.ArrayList;
 
 public class Ticket {
 
-	public double freeTicket(City bestCity,ArrayList<Traveller> user) {
+	public Traveller freeTicket(City bestCity,ArrayList<Traveller> user) {
 		double max = 0;
 		double tmp;
+		Traveller t = null;
 		for(Traveller pr1: user) {
 			if( (tmp = pr1.calculateSimilarity(bestCity) )>= max) {
 				max = tmp;
+				t = pr1;
+				
 			}
 		}
-		return max;
+		return t;
 	}
 	
 }
